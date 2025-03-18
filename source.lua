92e8abd9181747e679c94799ef490b237f7a5633
Guhttanin <naoseimds095@gmail.com>
2025-03-18 06:42:47 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,4 +1,4 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/RedzLibV5/refs/heads/main/Source.lua"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/RedzLibV5/main/Source.lua"))()
 
 local Window = redzlib:MakeWindow({
     Title = "SD-Goal Hub",
