975078d4adaa89641b4a340620c9f393ee6fd66b
Guhttanin <naoseimds095@gmail.com>
2025-03-19 03:31:15 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -207,56 +207,51 @@ end})
 
 AutoFarmBanco_tab:AddButton({"Teste", function()
     task.spawn(function()
-        if workspace.Map.Robberies.Bank.StatusDisplay.SurfaceGui.TextLabel.Text == "ABERTO" then
-            while true do
+        while true do 
+            if workspace.Map.Robberies.Bank.StatusDisplay.SurfaceGui.TextLabel.Text == "ABERTO" then
                 if workspace.Map.Robberies.Bank.BankArea.Alarm.Playing == true then
-                    while true do
-                        targetCFrame = CFrame.new(43.2352448, 16.2101593, 28.3578701, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                    targetCFrame = CFrame.new(43.2352448, 16.2101593, 28.3578701, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                    character:SetPrimaryPartCFrame(targetCFrame)
+                    wait(0.2)
+                    targetCFrame = CFrame.new(56.1759377, 16.3620834, 27.7989845, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                    character:SetPrimaryPartCFrame(targetCFrame)
+                    wait(0.2)
+                    local money_bag = workspace:FindFirstChild(player.Name):WaitForChild("Money Bag").Handle.DataAttachment.BillboardGui.Frame.Money
+                    local money_bag_value = tonumber(money_bag.Text:match("R%$(%d+)"))
+                    if money_bag_value >= 4000 then
+                        targetCFrame = CFrame.new(-590.812012, 31.3067017, 347.676727, 1, 0, 0, 0, 1, 0, 0, 0, 1)
                         character:SetPrimaryPartCFrame(targetCFrame)
-                        wait(0.2)
-                        targetCFrame = CFrame.new(56.1759377, 16.3620834, 27.7989845, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-                        character:SetPrimaryPartCFrame(targetCFrame)
-                        wait(0.2)
-                        local money_bag = workspace:FindFirstChild(player.Name):WaitForChild("Money Bag").Handle.DataAttachment.BillboardGui.Frame.Money
-                        local money_bag_value = tonumber(money_bag.Text:match("R%$(%d+)"))
-                        if money_bag_value >= 4000 then
-                            targetCFrame = CFrame.new(-590.812012, 31.3067017, 347.676727, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-                            character:SetPrimaryPartCFrame(targetCFrame)
-                            wait(0.2)
-                            VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.E, false, game)
-                            wait(0.2)
-                           local args = {
-                                [1] = "CollectCash"
-                           }
-                           game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("Robbery"):FireServer(unpack(args))
-                           wait(0.2)
-                        end
-                    end
-                elseif workspace.Map.Robberies.Bank.BankArea.Alarm.Playing == false then
-                    while not workspace.Map.Robberies.Bank.BankArea.Alarm do
-                        targetCFrame = CFrame.new(-921.370667, 49.0120926, 580.083923, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-                        character:SetPrimaryPartCFrame(targetCFrame)
-                        wait(0.2)
-                        local args = {
-                            [1] = "IlegalShop",
-                            [2] = "Buy",
-                            [3] = "C4"
-                        }
-                        game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("ClientPurchases"):FireServer(unpack(args))
-                        wait(0.2)
-                        local backpack = player:WaitForChild("Backpack")
-                        local tool = backpack:WaitForChild("C4")
-                        tool.Parent = character
-                        wait(0.2)
-                        targetCFrame = CFrame.new(60.9868698, 16.2101593, 41.6791344, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-                        character:SetPrimaryPartCFrame(targetCFrame)
-                        wait(0.2)
-                        workspace.Map.Robberies.Bank.VaultDoor.C4.Handle.ProximityPrompt.HoldDuration = 0
                         wait(0.2)
                         VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.E, false, game)
+                        wait(0.2)
+                        local args = {
+                            [1] = "CollectCash"
+                        }
+                        game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("Robbery"):FireServer(unpack(args))
+                        wait(0.2)
                     end
+                elseif workspace.Map.Robberies.Bank.BankArea.Alarm.Playing == false then
+                    targetCFrame = CFrame.new(-921.370667, 49.0120926, 580.083923, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                    character:SetPrimaryPartCFrame(targetCFrame)
+                    wait(0.2)
+                    local args = {
+                        [1] = "IlegalShop",
+                        [2] = "Buy",
+                        [3] = "C4"
+                    }
+                    game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("ClientPurchases"):FireServer(unpack(args))
+                    wait(0.2)
+                    local backpack = player:WaitForChild("Backpack")
+                    local tool = backpack:WaitForChild("C4")
+                    tool.Parent = character
+                    wait(0.2)
+                    targetCFrame = CFrame.new(60.9868698, 16.2101593, 41.6791344, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                    character:SetPrimaryPartCFrame(targetCFrame)
+                    wait(0.2)
+                    workspace.Map.Robberies.Bank.VaultDoor.C4.Handle.ProximityPrompt.HoldDuration = 0
+                    wait(0.2)
+                    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.E, false, game)
                 end
-            wait(0.5)
             end
         end
     end)
